e546d5e272c974b5e08b9ca6780e05578bd77556
ChrisBug <Chris.Buggy@icloud.com>
2022-07-09 23:01:04 +0100

    did some stuff

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,8 @@
 package guru.springframework.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -10,6 +12,8 @@ import java.util.Set;
  * Created by jt on 6/13/17.
  */
 @Data
+@Getter
+@Setter
 @Entity
 public class Recipe {
 
