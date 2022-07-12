fd9056b45c293d76696d041213102bf89b4d0c81
ChrisBug <Chris.Buggy@icloud.com>
2022-07-12 23:34:48 +0100

    index controller test

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,6 +1,10 @@
 package guru.springframework.controllers;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,30 +13,28 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
 import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.services.RecipeService;
 import guru.springframework.services.RecipeServiceImpl;
 
 public class IndexControllerTest {
 
-    RecipeServiceImpl recipeService;
     IndexController indexController;
-
-    @Mock
-    RecipeRepository recipeRepository;
     @Mock
     Model model;
+    @Mock
+    RecipeService recipeService;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        recipeService = new RecipeServiceImpl(recipeRepository);
         indexController = new IndexController(recipeService);
-        model.addAttribute("Test", true);
     }
 
 
     @Test
     public void testGetIndexPage() {
         assertEquals("index", indexController.getIndexPage(model));
-        assertEquals(true, model.containsAttribute("Test"));
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
     }
 }
